aae15683bb0b29a9cee485e9a329291cb4ef9641
Giselle Balieiro <gisellefbalieiro@outlook.com>
2025-02-21 14:18:43 -0300

    feat: exibir produtos e limpar lista com botão

--- src/Pages/ListProduct.jsx
+++ src/Pages/ListProduct.jsx
@@ -1,7 +1,24 @@
 import React from "react"
 
-const ListProduct = () => {
+const ListProduct = ({ product, setProduct}) => {
+    const handleRemove = (indexHandleRemove) => {
+        setProduct(product.filter((_,index) => index !== indexHandleRemove))
+    }
+    
+    console.log("Produtos a serem listados:", product);
 
+    return (
+        <ul>
+            {product.map((product, index) => (
+                <li key={index}>
+                    <p>Nome: {product.name}</p>
+                    <p>Preço: {product.price}</p>
+                    <p>Quantidade: {product.quantity}</p>
+                    <button onClick={() => handleRemove(index)}>Remover</button>
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default ListProduct;

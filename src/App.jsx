32099f9a7c27309a8d2a57c7e5abc73534fb8512
Giselle Balieiro <gisellefbalieiro@outlook.com>
2025-02-21 14:20:02 -0300

    feat: adicionar useEffect para salvar produtos no LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import './App.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import ListProduct from './Pages/ListProduct'
 import ProductRegistration from './Pages/ProductRegistration'
+import { useEffect, useState } from 'react';
 
 
 function App() {
-  return (
-      <Router>
-         <Routes>
-            <Route path="/" element={<ProductRegistration/>} />
-            <Route path="/products" element={<ListProduct/>} />
-         </Routes>
-      </Router>
+   const [product, setProduct] = useState([]);
+
+   useEffect(() => {
+      const savedProduct = localStorage.getItem("product");
+      if (savedProduct) {
+         setProduct(JSON.parse(savedProduct));
+      }
+   }, []);  
+
+   useEffect(() => {
+      if(product.length > 0)
+      localStorage.setItem("product", JSON.stringify(product));
+   }, [product]);
+
+   return (
+         <Router>
+            <Routes>
+               <Route path="/" element={<ProductRegistration product={product} setProduct={setProduct}/>} />
+               <Route path="/products" element={<ListProduct product={product} setProduct={setProduct}/>} />
+            </Routes>
+         </Router>
   )
 }
 

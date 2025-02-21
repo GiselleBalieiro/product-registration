8443eb9bd5989c143e39ecacb8e1e487aa66ba22
Giselle Balieiro <gisellefbalieiro@outlook.com>
2025-02-21 14:17:23 -0300

    feat: adicionar novo produto ao enviar formulário

--- src/Pages/ProductRegistration.jsx
+++ src/Pages/ProductRegistration.jsx
@@ -3,22 +3,31 @@ import React, { useState } from "react"
 import Input from "../Components/Input";
 import Button from "../Components/Button";
 
-const ProductRegistration = () => {
+const ProductRegistration = ({product, setProduct}) => {
     const [name, setName] = useState("");
     const [price, setPrice ] = useState("");
     const [quantity, setQuantity] = useState("");
 
-    const handleClick = (event) =>{
+    const handleSubmit = (event) =>{
         event.preventDefault();
+        const newProduct = {name, price, quantity};
+        setProduct([...product, newProduct])
+    
         console.log("Produto cadastrado com sucesso!", name, price, quantity);
-    }
+        console.log("Produtos cadastrados: ", newProduct);
+        
+        setName("");
+        setPrice("");   
+        setQuantity("");
 
+    }
+    
     return (
-        <form>
-            <Input label="Nome: " type="text" placeholder="Nome do produto" onChange={(event) => setName(event.target.value)} />
-            <Input label="Preço: " type="number" placeholder="Preço do produto" onChange={(event) => setPrice(event.target.value)} />
-            <Input label="Quantidade: " type="number" placeholder="Quantidade do produto" onChange={(event) => setQuantity(event.target.value)} />
-            <Button onClick={handleClick}>Salvar</Button>
+        <form onSubmit={handleSubmit}>
+            <Input label="Nome: " type="text" value={name} placeholder="Nome do produto" onChange={(event) => setName(event.target.value)} />
+            <Input label="Preço: " type="number" value={price} placeholder="Preço do produto" onChange={(event) => setPrice(event.target.value)} />
+            <Input label="Quantidade: " type="number" value={quantity} placeholder="Quantidade do produto" onChange={(event) => setQuantity(event.target.value)} />
+            <Button type="submit">Salvar</Button>
         </form>
     )
 }

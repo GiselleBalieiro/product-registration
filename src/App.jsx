363a3fd1fb8cd052b73ca037c04d5cc292151e5d
Giselle Balieiro <gisellefbalieiro@outlook.com>
2025-02-24 10:36:55 -0300

    feat: criação de componente e implementação no App

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,38 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import ListProduct from './Pages/ListProduct'
 import ProductRegistration from './Pages/ProductRegistration'
 import { useEffect, useState } from 'react';
-
+import Sidebar from './Components/Sidebar';
 
 function App() {
-   const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState([]);
 
-   useEffect(() => {
-      const savedProduct = localStorage.getItem("product");
-      if (savedProduct) {
-         setProduct(JSON.parse(savedProduct));
-      }
-   }, []);  
+  useEffect(() => {
+    const savedProduct = localStorage.getItem("product");
+    if (savedProduct) {
+      setProduct(JSON.parse(savedProduct));
+    }
+  }, []);
 
-   useEffect(() => {
-      if(product.length > 0)
+  useEffect(() => {
+    if (product.length > 0)
       localStorage.setItem("product", JSON.stringify(product));
-   }, [product]);
+  }, [product]);
 
-   return (
-         <Router>
-            <Routes>
-               <Route path="/" element={<ProductRegistration product={product} setProduct={setProduct}/>} />
-               <Route path="/products" element={<ListProduct product={product} setProduct={setProduct}/>} />
-            </Routes>
-         </Router>
+  return (
+    <Router>
+      <div className='flex'>
+        <div className='flex-1 p-6'>
+          <Sidebar />
+        </div>
+        <div className='flex-3 p-6'>
+          <Routes>
+            <Route path="/" element={<ProductRegistration product={product} setProduct={setProduct} />} />
+            <Route path="/register" element={<ProductRegistration product={product} setProduct={setProduct} />} />
+            <Route path="/products" element={<ListProduct product={product} setProduct={setProduct} />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 

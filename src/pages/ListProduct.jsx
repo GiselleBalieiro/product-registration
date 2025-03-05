413af46122b81c0b4544ae4302466ab55c51ac79
Giselle Balieiro <gisellefbalieiro@outlook.com>
2025-03-05 16:53:15 -0300

    chore: remove importação desnecessária

--- src/pages/ListProduct.jsx
+++ src/pages/ListProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import Button from "../component/Button";
 import { Link } from "react-router-dom";
-import FilterSearch from "../component/FilterSearch";
+
 const ListProduct = ({ product, setProduct, productEdit}) => {
     const [filterList, setFilterList] = useState("")
 
